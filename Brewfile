9ddd3c92a8dba8f01a9d749470df089bbeda96a2
Scott Wilcox <scott@dor.ky>
2017-04-17 18:38:45 +0100

    Initial commit

---
+++ Brewfile
@@ -0,0 +1,108 @@
+cask_args appdir: '/Applications'
+
+tap 'caskroom/cask'
+tap 'caskroom/fonts'
+tap 'caskroom/versions'
+tap 'homebrew/bundle'
+tap 'homebrew/dupes'
+tap 'homebrew/php'
+tap 'homebrew/services'
+
+# Install binaries
+brew 'htop'
+brew 'wget'
+brew 'git'
+brew 'ncdu'                     # Usual disk measurement tool
+brew 'homebrew/php/phpunit'     # phpunit
+brew 'diff-so-fancy'            # Better formatting for diff's
+brew 'gpg'                      # GPG Encryption
+brew 'ansible@2'                # Ansible for bedrock/wordpress
+brew 'acmetool'                 # Let's Encrypt tool
+brew 'youtube-dl'
+brew 'awscli'
+brew 'hub'
+brew 'tree'
+brew 'mackup'
+brew 'mas'
+brew 'keybase'
+
+# Install PHP7
+brew 'php71', args: ['with-debug']
+brew 'php71-intl'
+
+# Install ZSH
+brew 'zsh'
+brew 'zsh-completions'
+
+# Install GNU core utilities (those that come with macOS are outdated)
+brew 'coreutils'
+
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
+brew 'findutils'
+
+link 'php71'
+
+cask 'google-chrome'
+cask 'vlc'
+cask 'dropbox'
+cask 'quicklook-csv'
+cask 'provisionql'
+cask 'quicklookapk'
+cask 'qlmarkdown'
+cask 'fastlane'
+
+# Apps
+cask 'caffeine'
+cask 'dropbox'
+cask 'firefox'
+cask 'google-chrome'
+cask 'phpstorm'
+cask 'sequel-pro'
+cask 'sublime-text'
+cask 'tower'
+cask 'transmit'
+cask 'vagrant'
+cask 'virtualbox'
+cask 'virtualbox-extension-pack'
+cask 'vlc'
+
+# Install Mac App Store apps
+mas '1Password', id: 443987910
+mas 'Paw', id: 584653203
+mas 'Tweetbot', id: 557168941
+
+
+# laravel valet
+# Audio Hijack
+# Docker for Mac
+# Atom
+# GenyMotion
+# Freeze
+# Franz
+# Knox
+# Microsoft Office
+# XCode
+# Atom
+# CleanMyMac
+# VMware Fusion (Install Windows 7 Box too)
+# The Unarchiver
+# Sequel Pro
+# Tower
+# Traffic
+# Transmission
+# Ulysses
+# Virtualbox
+# Paw
+# Paparazzi
+# Moom
+# Permute
+# PCalc
+# Just Press Record
+# Kaleidoscope
+# Docker for Mac
+# Things
+# GPG Keychain
+# Growl
+# Bowtie
+# Appcelerator Studio
+# Adobe Air
---
+++ install.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Change some defaults
+defaults write NSNavPanelExpandedStateForSaveMode -bool true
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+defaults write com.apple.ImageCapture disableHotPlug -bool true
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
+defaults write NSGlobalDomain com.apple.springing.enabled -bool true
+defaults write NSGlobalDomain com.apple.springing.delay -float 0
+defaults write com.apple.Safari AutoOpenSafeDownloads -bool false
+defaults write com.apple.loginwindow TALLogoutSavesState 0
+
+# Set the library folder as hidden
+chflags nohidden ~/Library/
+
+# Don’t let Mac start on opening lid
+sudo nvram AutoBoot=%00
+sudo nvram BootAudio=%00
+pmset -a hibernatemode 0
+rm /var/vm/sleepimage
+
+# Install Xcode tools
+xcode-select --install
+
+# Copy dotfiles over into user home directory
+ln -s ~/Dropbox/.zshrc ~/.zshrc
+ln -s ~/Dropbox/.gitconfig ~/.gitconfig
+ln -s ~/Dropbox/.gitignore ~/.gitignore
+ln -s ~/Dropbox/.ssh ~/.ssh
+
+# Install homebrew and homebrew packages
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+cd ~/Dropbox/dotfiles
+brew bundle
+cd ~
+gem install bundler
+
+# Install Composer
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"\
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '669656bab3166a7aff8a7506b8cb2d1c292f042046c5a994c43155c0be6190fa0355160742ab2e1c88d40d5be660b410') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"\
+php composer-setup.php\
+php -r "unlink('composer-setup.php');"\
+sudo mv composer.phar /usr/local/bin/composer
+
+# Install Laravel Valet
+composer global require laravel/valet
+valet install
+valet domain valet
+sudo brew services stop --all
+
+# Install NPM modules
+sudo npm install -g bower yarn cssunminifier gulp purify-css jasmine
